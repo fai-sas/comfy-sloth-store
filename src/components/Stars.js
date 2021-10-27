f7e63ac9a726a8cb837abcb2432a865aca35d843
fai-sas <craftedbyfaisal@gmail.com>
2021-10-27 20:57:21 +0600

    single product - stars (manual approach)

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,8 +1,74 @@
 import React from 'react'
 import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
+import { BsStarFill, BsStarHalf, BsStar, BsStop } from 'react-icons/bs'
+
+const Stars = ({ stars, reviews }) => {
+  return (
+    <Wrapper>
+      <div className='stars'>
+        {/* Star 1 */}
+        <span>
+          {stars >= 1 ? (
+            <BsStarFill />
+          ) : stars >= 0.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        {/* End of Star 1*/}
+
+        {/* Star 2 */}
+        <span>
+          {stars >= 2 ? (
+            <BsStarFill />
+          ) : stars >= 1.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        {/* End of Star 2*/}
+
+        {/* Star 3 */}
+        <span>
+          {stars >= 3 ? (
+            <BsStarFill />
+          ) : stars >= 2.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        {/* End of Star 3*/}
+
+        {/* Star 4 */}
+        <span>
+          {stars >= 4 ? (
+            <BsStarFill />
+          ) : stars >= 3.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        {/* End of Star 4*/}
+
+        {/* Star 5 */}
+        <span>
+          {stars === 5 ? (
+            <BsStarFill />
+          ) : stars >= 4.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        {/* End of Star 5*/}
+      </div>
+      <p className='reviews'>({reviews} customer reviews)</p>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
