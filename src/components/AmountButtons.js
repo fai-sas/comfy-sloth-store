2a9acbd00e80068759bbb123ede6a3425e94ef6e
fai-sas <craftedbyfaisal@gmail.com>
2021-10-30 20:41:51 +0600

    single product - add to cart - amount buttons

--- src/components/AmountButtons.js
+++ src/components/AmountButtons.js
@@ -2,8 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 import { FaPlus, FaMinus } from 'react-icons/fa'
 
-const AmountButtons = () => {
-  return <h4>amount buttons </h4>
+const AmountButtons = ({ increase, decrease, amount }) => {
+  return (
+    <Wrapper>
+      <button type='button' onClick={decrease}>
+        <FaMinus />
+      </button>
+
+      <h2>{amount}</h2>
+
+      <button type='button' onClick={increase}>
+        <FaPlus />
+      </button>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
