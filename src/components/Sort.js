ab13d4a4d89dbb915c19f07326ee910639349019
fai-sas <craftedbyfaisal@gmail.com>
2021-11-03 21:00:01 +0600

    sort component-controlled input

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -9,6 +9,8 @@ const Sort = () => {
     grid_view,
     setGridView,
     setListView,
+    sort,
+    updateSort,
   } = useFilterContext()
 
   return (
@@ -35,7 +37,13 @@ const Sort = () => {
 
       <form>
         <label htmlFor='sort'>sort by</label>
-        <select name='sort' id='sort' className='sort-input'>
+        <select
+          name='sort'
+          id='sort'
+          className='sort-input'
+          value={sort}
+          onChange={updateSort}
+        >
           <option value='price-lowest'>price(lowest)</option>
           <option value='price-highest'>price(highest)</option>
           <option value='name-a'>name(a-z)</option>
