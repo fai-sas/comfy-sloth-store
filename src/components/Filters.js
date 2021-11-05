69a4adb253a703135837ff6d16d4f8c35138d0af
fai-sas <craftedbyfaisal@gmail.com>
2021-11-05 19:02:16 +0600

    filters-companies

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -43,6 +43,8 @@ const Filters = () => {
             />
           </div>
           {/* end of search input */}
+
+          {/* categories */}
           <div className='form-control'>
             <h5>category</h5>
             <div>
@@ -63,8 +65,29 @@ const Filters = () => {
               })}
             </div>
           </div>
-          {/* categories */}
           {/* end of categories */}
+
+          {/* companies */}
+
+          <div className='form-control'>
+            <h5>company</h5>
+            <select
+              name='company'
+              value={company}
+              onChange={updateFilters}
+              className='company'
+            >
+              {companies.map((c, index) => {
+                return (
+                  <option key={index} value={c}>
+                    {c}
+                  </option>
+                )
+              })}
+            </select>
+          </div>
+
+          {/* end of companies */}
         </form>
       </div>
     </Wrapper>
