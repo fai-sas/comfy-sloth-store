1a5b620cb05dfd3fb996dfe641b627500c966c4f
fai-sas <craftedbyfaisal@gmail.com>
2021-11-13 20:39:34 +0600

    cart-toggle amount

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -6,10 +6,15 @@ import { FaTrash } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
 
 const CartItem = ({ id, image, name, color, price, amount }) => {
-  const { removeItem, toggleItem } = useCartContext()
-  const increase = () => {}
+  const { removeItem, toggleAmount } = useCartContext()
 
-  const decrease = () => {}
+  const increase = () => {
+    toggleAmount(id, 'inc')
+  }
+
+  const decrease = () => {
+    toggleAmount(id, 'dec')
+  }
 
   return (
     <Wrapper>
