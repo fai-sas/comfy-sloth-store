155d3af323ff085333bb3beb2dfdb4c687537cd3
fai-sas <craftedbyfaisal@gmail.com>
2021-11-15 21:10:13 +0600

    useContext setup

--- src/index.js
+++ src/index.js
@@ -8,14 +8,29 @@ import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
+//domain
+// dev-pu8wyk-g.us.auth0.com
+
+//Client ID
+// 7vHgXJ01aWGyMVLWdZUwQSUapigdjfKu
+
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain='dev-pu8wyk-g.us.auth0.com'
+    clientId='7vHgXJ01aWGyMVLWdZUwQSUapigdjfKu'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
 
   document.getElementById('root')
 )
